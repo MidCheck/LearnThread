eda0579f9369b7888285a16daa588fcb9bf4078b
MidCheck <MidCheck@foxmail.com>
2019-05-25 19:25:50 +0800

    delete operator= to avoid side effects

--- include/LinkNode.h
+++ include/LinkNode.h
@@ -2,11 +2,12 @@
 #ifndef LINKNODE_H
 #define LINKNODE_H
 #include "StdAfx.h"
+
 #ifndef _TEMPLATE
 struct Node
 {
 	//对于对象的初始化，都通过构造函数进行
-	Node(int data = 0) :mdata(data), mpnext(NULL) {}
+	Node(int data = 0) :mdata(data), mpnext(nullptr) {}
 	Node(Node& node) :mdata(node.mdata), mpnext(node.mpnext) {}
 	bool operator==(const Node& node) {return this == &node;}
 	bool operator!=(const Node& node) { return !this->operator==(node); }
@@ -16,14 +17,16 @@ struct Node
 #else
 template <typename ElemType>
 struct Node {
-	//对于对象的初始化，都通过构造函数进行
-	Node() :mpnext(NULL) {}
-	Node(ElemType& data) :mdata(data), mpnext(NULL) {}
-	Node(Node& node) :mdata(node.mdata), mpnext(node.mpnext) {}
-	bool operator==(const Node& node) { return this == &node; }
-	bool operator!=(const Node& node) { return !this->operator==(node); }
 	ElemType mdata;
 	Node* mpnext;
+
+	//对于对象的初始化，都通过构造函数进行
+	Node() :mpnext(nullptr) {}
+	Node(const ElemType& data) :mdata(data), mpnext(nullptr) {}
+	Node(const Node& node) :mdata(node.mdata), mpnext(node.mpnext) {}
+	Node& operator=(const Node&) = delete; 
+	bool operator==(const Node& node) { return this == &node; }
+	bool operator!=(const Node& node) { return !this->operator==(node); }
 };
 #endif
 
