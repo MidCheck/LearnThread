f3881c2daec136a07f2a6e34f9277f1d3cf7e273
MidCheck <MidCheck@foxmail.com>
2019-05-28 14:36:25 +0800

    removed the definition of non-template class

--- include/LinkNode.h
+++ include/LinkNode.h
@@ -3,18 +3,6 @@
 #define LINKNODE_H
 #include "StdAfx.h"
 
-#ifndef _TEMPLATE
-struct Node
-{
-	//对于对象的初始化，都通过构造函数进行
-	Node(int data = 0) :mdata(data), mpnext(nullptr) {}
-	Node(Node& node) :mdata(node.mdata), mpnext(node.mpnext) {}
-	bool operator==(const Node& node) {return this == &node;}
-	bool operator!=(const Node& node) { return !this->operator==(node); }
-	int mdata;
-	Node* mpnext;
-};
-#else
 template <typename ElemType>
 struct Node {
 	ElemType mdata;
@@ -28,6 +16,5 @@ struct Node {
 	bool operator==(const Node& node) { return this == &node; }
 	bool operator!=(const Node& node) { return !this->operator==(node); }
 };
-#endif
 
 #endif
